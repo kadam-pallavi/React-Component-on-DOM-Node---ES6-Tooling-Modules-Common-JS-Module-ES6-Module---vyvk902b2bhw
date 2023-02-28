ce858adb00f6d7a05d6056998df8d5e3165cee58
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-03-01 00:25:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ function App() {
     <p>
     ReactDOM.render
       Now I can render any React component on any DOM node I want using
+    ReactDOM.render(document.getElementById("root"));
     </p>
   );
 }
