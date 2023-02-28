559943cffcca5ffb42acee7bc573a58c2233126c
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-03-01 00:23:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import "../styles/App.css";
 function App() {
   return (
     <p>
+    ReactDOM.render
       Now I can render any React component on any DOM node I want using
-      ReactDOM.render
     </p>
   );
 }
